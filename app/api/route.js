f555c10bb1b9404f26cd337d570be415e9a0cce5
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-12-19 16:28:34 +0200

    added api/create task

---
+++ app/api/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+import Task from "../(models)/Task";
+
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const taskData = body.formData;
+
+    await Task.create(taskData);
+
+    return NextResponse.json({ message: "Task created" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
