003acab6284bb61483229f929b61eb801c7b36d1
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-12-19 15:30:48 +0200

    added comps

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,16 @@
+import TaskCard from "./(components)/TaskCard";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- app/task/[id]/page.jsx
+++ app/task/[id]/page.jsx
@@ -1,5 +1,5 @@
 const Task = ({ params }) => {
-  return <div>Task ${params.id}</div>;
+  return <div>Task {params.id}</div>;
 };
 
 export default Task;
